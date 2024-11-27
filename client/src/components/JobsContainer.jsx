ce22142ed6779b7a4844e8f0af784f8a1268f9de
Ketoran Sileshi <abdisileshi123@gmail.com>
2024-11-27 19:04:12 +0300

    fix pageBtnContainer import

--- client/src/components/JobsContainer.jsx
+++ client/src/components/JobsContainer.jsx
@@ -1,7 +1,7 @@
 import Job from "./Job";
 import Wrapper from "../assets/wrappers/JobsContainer";
 import { useAllJobsContext } from "../pages/AllJobs";
-import PageBtnContainer from "./pageBtnContainer";
+import PageBtnContainer from "./PageBtnContainer";
 
 const JobsContainer = () => {
   const { data } = useAllJobsContext();
